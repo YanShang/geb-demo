1b3c1769224232f2fda0106a3f01268046035e63
YanShang <yuyanshang@gmail.com>
2014-09-21 14:47:31 -0700

    Fix indentations

--- src/test/groovy/MySpec.groovy
+++ src/test/groovy/MySpec.groovy
@@ -8,42 +8,42 @@ class MySpec extends GebReportingSpec {
 
         and: 
         interact {
-			loginForm.j_username = "xxx"
-			loginForm.j_password = "xxx"
-			loginButton.click()
+            loginForm.j_username = "xxx"
+            loginForm.j_password = "xxx"
+            loginButton.click()
         }
 
         then: 
-		at HomePage
+        at HomePage
 
         when: 
-		searchLinks.find("a", text:"Bill of Materials").click()
+        searchLinks.find("a", text:"Bill of Materials").click()
 
         then:
         at SearchPage
-		
+
         when: 	
-		itemNumber = "SHN9663B"
-		region.find("option", text:"Bom Source MotoPDM").click()
-		searchButton.click()
-		
-		then:
-		at ResultPage
-		
-		when:
-		searchResults.find("a", text:"SHN9663B") << org.openqa.selenium.Keys.ENTER
-		//searchResults.find("a", text:"SHN9663B").click()
-		
-		then:
-		at BomInfoPage
-		$("a", text:"Flat BOM").size() == 1	
-		
-		when:
-		$("a", text:"Flat BOM").click()
-		
-		then:
-		at BomInfoPage
-		$("span", text:"Flat BOM").size() == 1		
-		$("div", text:"0387347Y01").size() == 1 
+        itemNumber = "SHN9663B"
+        region.find("option", text:"Bom Source MotoPDM").click()
+        searchButton.click()
+
+        then:
+        at ResultPage
+
+        when:
+        searchResults.find("a", text:"SHN9663B") << org.openqa.selenium.Keys.ENTER
+        //searchResults.find("a", text:"SHN9663B").click()
+
+        then:
+        at BomInfoPage
+        $("a", text:"Flat BOM").size() == 1	
+ 
+        when:
+        $("a", text:"Flat BOM").click()
+
+        then:
+        at BomInfoPage
+        $("span", text:"Flat BOM").size() == 1		
+        $("div", text:"0387347Y01").size() == 1 
     }
 }
